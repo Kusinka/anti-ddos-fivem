7a76ee1ac31c06db2eb1456088a4fc6684468838
Kusinka <83094308+Kusinka@users.noreply.github.com>
2024-01-14 20:55:16 +0100

    Add files via upload

---
+++ SafeShieldAnti-DDOS/config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.MaxConnectionsPerIP = 3 -- Connection limit every 5 minutes.
+Config.DiscordWebhook = "https://discord.com/api/webhooks/XXXXXXX/XXXXXXX" -- Your Webhook URL
---
+++ SafeShieldAnti-DDOS/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'kusinka'
+description 'Anti DDoS Uxu'
+version '1.0.0'
+
+server_script 'config.lua'
+server_script 'server.lua'
+
